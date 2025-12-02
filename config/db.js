b3405ab4a5baa2ced66288ac5f610af5c476ee73
jahangirahmed77 <jahangeer9182@gmail.com>
2025-12-02 20:23:08 +0500

    vvv

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://userauth:auth1234@cluster0.n36oipi.mongodb.net/');
+    await mongoose.connect('mongodb+srv://user:user@cluster0.wascy33.mongodb.net/');
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.log("error while connecting to mongoDB", error);
